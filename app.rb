997c209d37f781e63e9cab095d65d326f9931d46
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-25 20:40:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do 
-    "#{params[:name]} #{params[:favorite_food]}"
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 
 end
